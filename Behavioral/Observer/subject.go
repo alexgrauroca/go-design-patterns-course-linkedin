801c1398ff75219f4de7a97c99a0dd31f2cebaf3
Àlex Grau Roca <alexgrauroca91@gmail.com>
2023-09-25 23:02:21 +0200

    Observer implemented

---
+++ Behavioral/Observer/subject.go
@@ -0,0 +1,37 @@
+package main
+
+type observable interface {
+	registerObserver(obs observer)
+	unregisterObserver(obs observer)
+	notifyAll()
+}
+
+type DataSubject struct {
+	observers []DataListener
+	field     string
+}
+
+func (ds *DataSubject) ChangeItem(data string) {
+	ds.field = data
+	ds.notifyAll()
+}
+
+func (ds *DataSubject) registerObserver(o DataListener) {
+	ds.observers = append(ds.observers, o)
+}
+
+func (ds *DataSubject) unregisterObserver(o DataListener) {
+	var newObs []DataListener
+	for _, obs := range ds.observers {
+		if o.Name != obs.Name {
+			newObs = append(newObs, obs)
+		}
+	}
+	ds.observers = newObs
+}
+
+func (ds *DataSubject) notifyAll() {
+	for _, obs := range ds.observers {
+		obs.onUpdate(ds.field)
+	}
+}
